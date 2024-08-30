90fa674ed7749ec968b7f649f044369336520c10
otabekotamurodov <105487505+otabekotamurodov@users.noreply.github.com>
2024-08-30 17:11:44 +0500

    1

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from functions import daryo_articles, daryo_article
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     articles = daryo_articles()
     return render_template('index.html', articles=articles)
+
 
 @app.route('/article')
 def article():
@@ -15,6 +17,7 @@ def article():
     image_link = request.args.get('image')
     article_data = daryo_article(title=title, link=url, image_link=image_link)
     return render_template('article.html', article=article_data)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
